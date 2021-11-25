2da4c6b31747884d452f30097ee82442766f6854
naveen <1998naveennavi@gmail.com>
2021-11-25 09:48:41 +0530

    2nd commit

---
+++ AssignmentPrograms/EvenNum.java
@@ -0,0 +1,13 @@
+package com.technoelevate.practiceprograms;
+
+public class EvenNum {
+	public static void main(String[] args) {
+		int a;
+		a = 10;
+		for(int i=0;i<=a;i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
---
+++ AssignmentPrograms/OddNum.java
@@ -0,0 +1,13 @@
+package com.technoelevate.practiceprograms;
+
+public class OddNum {
+	public static void main(String[] args) {
+		int a;
+		a = 10;
+		for(int i=0;i<=a;i++) {
+			if(i%2 == 1) {
+				System.out.println(i);
+			}
+		}
+	}
+}
